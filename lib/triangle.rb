d5c263e6e3b645de28368ddaa2fb1722dacb03de
Nory Lazo <nlazo@icloud.com>
2019-12-16 06:55:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,8 @@ class Triangle
     
     if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 
       raise TriangleError
-    elsif @
+    elsif @side_1 + @side_2 < @side_3 || @side_2 + @side_3 < @side_1
+      raise TriangleError
     end
     
     if @side_1 == @side_2 && @side_1 == @side_3 
